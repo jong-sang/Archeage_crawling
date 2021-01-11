abc6272645e34c674f7ba36bae3a9273572dee19
박종상 <jongsangkuun@Macbook.local>
2021-01-11 11:25:41 +0900

    디코봇 연동 및 리턴값 수정

--- Crawl.py
+++ Crawl.py
@@ -9,57 +9,111 @@ import urllib3
 
 
 class Bond:
-
     def Bond_server(self,target):
         group = ['table-bond','table-bond right']
         url = requests.get(target)
         soup = BeautifulSoup(url.content,"html.parser")
+        result = '```\n'
+        cloth = ''
+        leather = ''
+        wood = ''
+        iron = ''
+
         for i in group:
-            if(i == 'table-bond'):
-                print('서대륙')
-            if(i == 'table-bond right'):
-                print('동대륙')
+            # if(i == 'table-bond'):
+            #     continent = '-서대륙'
+            #     # print('서대륙')
+            # if(i == 'table-bond right'):
+            #     # print('동대륙')
+            #     continent = '+동대륙'
 
             point = soup.find('table',class_= i)
             trs = point.tbody.find_all('tr')
             items = ['옷감','가죽','목재','철 주괴']
 
             for j in range(0,4):
-                print(items[j])
+                # print(items[j])
                 try:
                     rank = trs[j].find('li',class_='point').string
-                    print(rank)
+                    if items[j] == items[0]:
+                        cloth += rank
+                        cloth += '\n'
+                    elif items[j] == items[1]:
+                        leather += rank
+                        leather += '\n'
+                    elif items[j] == items[2]:
+                        wood += rank
+                        wood += '\n'
+                    elif items[j] == items[3]:
+                        iron += rank  
+                        iron += '\n' 
+                    else:
+                        print('') 
+                    # 
+                    # print(rank)
+                    # result += items[j]
+                    # result += '\n'
+                    # result += rank
+                    # result += '\n'
+                    # result += '\n'
                     print('')
+                    
                 except:
                     print('')
+        for z in items:
+            result += z
+            result += '\n'
+            if z == items[0]:
+                result += cloth
+                result += '\n'
+            if z == items[1]:
+                result += leather
+                result += '\n'
+            if z == items[2]:
+                result += wood
+                result += '\n'
+            if z == items[3]:
+                result += iron
+                result += '\n'
+
+        result += '```'
+        return result
 
 
     def Check_Server(self,server):
         url = 'https://archeage.xlgames.com/play/worldinfo/'
         if(server == '다후타'):
             target = url + 'DAHUTA'
-            Bond().Bond_server(target)
+            result = Bond().Bond_server(target)
+            return result
 
         elif(server == '누이'):
             target = url + 'NUI'
-            print(target)
-            Bond().Bond_server(target)
+            result = Bond().Bond_server(target)
+            return result
 
         elif(server == '하제'):
             target = url + 'HAJE'
-            Bond().Bond_server(target)
+            result = Bond().Bond_server(target)
+            return result
 
         elif(server == '정원'):
             target = url + 'GARDEN'
-            Bond().Bond_server(target)
+            result = Bond().Bond_server(target)
+            return result
 
         elif(server == '정원2'):
             target = url + 'GARDEN2'
-            Bond().Bond_server(target)
+            result = Bond().Bond_server(target)
+            return result
 
         elif(server == '모르페우스'):
             target = url + 'MORPHEUS'
-            Bond().Bond_server(target)      
+            result = Bond().Bond_server(target)
+            return result     
 
         else:
             print('error')
+
+a = Bond().Check_Server('다후타')
+
--- app.py
+++ app.py
@@ -1,3 +1,51 @@
 from Crawl import Bond
+import discord
+import asyncio
 
-Bond().Check_Server('누이')
+client = discord.Client()
+token = '???'
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    channel = message.channel
+
+    if message.content == '=test':
+        await channel.send('test~!')
+    
+    if message.content == '=채권 누이':
+        abc = Bond().Check_Server('누이')
+        await channel.send(abc)
+
+    if message.content == '=채권 다후타':
+        abc = Bond().Check_Server('다후타')
+        await channel.send(abc)
+
+    if message.content == '=채권 모르페우스':
+        abc = Bond().Check_Server('모르페우스')
+        await channel.send(abc)
+
+    if message.content == '=채권 정원':
+        abc = Bond().Check_Server('정원')
+        await channel.send(abc)
+
+    if message.content == '=채권 정원2':
+        abc = Bond().Check_Server('정원2')
+        await channel.send(abc)
+
+    if message.content == '=채권 하제':
+        abc = Bond().Check_Server('하제')
+        await channel.send(abc)
+
+
+
+client.run(token)
+
+# Bond().Check_Server('누이')
